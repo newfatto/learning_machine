d46926d143d77f33dfd97d3660488089093a4988
Ekaterina Chernyshova <newfatto@gmail.com>
2026-02-11 19:42:34 +0300

    Модель пользователя зарегистрирована в админке

---
+++ lms/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Курс: название, превью (картинка), описание.
+
+
+
+# class Course(models.Model):
+#
+#
+#     class Meta:
+#         verbose_name = 'курс'
+#         verbose_name_plural = 'курсы'
+
+
+
+# Урок:    название,    описание,    превью(картинка),    ссылка    на    видео.
