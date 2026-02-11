b165a6def92d4bd91efe25ddde9d3cc90ca809f0
Ekaterina Chernyshova <newfatto@gmail.com>
2026-02-11 20:46:06 +0300

    Модели курса и урока зарегистрированы в админке, проведены миграции

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("name",)
