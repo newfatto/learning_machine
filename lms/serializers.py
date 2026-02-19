c25cf03ebb734025a927529857aab7e988dd8699
Ekaterina Chernyshova <newfatto@gmail.com>
2026-02-19 13:19:14 +0300

    Добавлено отображение количества уроков в курсе (кастомный сериализатор CourseDetailSerializer и переопределён метод get_serializer_class в CourseViewSet)

--- lms/serializers.py
+++ lms/serializers.py
@@ -10,6 +10,22 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class CourseDetailSerializer(serializers.ModelSerializer):
+
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, course):
+        return Lesson.objects.filter(course=course.id).count()
+
+    class Meta:
+        model = Course
+        fields = (
+            "name",
+            "description",
+            "lesson_count",
+        )
+
+
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
