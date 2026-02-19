25880fdfe0123cacd70fbb18b1504774d8667c88
Ekaterina Chernyshova <newfatto@gmail.com>
2026-02-19 14:47:19 +0300

    Прописаны сериаизатор и контроллеры для Payment

--- users/views.py
+++ users/views.py
@@ -1,11 +1,35 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework import viewsets, generics
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('lesson', 'course', 'payment_way')
+    ordering_fields = ('payment_date',)
+
+
+class PaymentRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+class PaymentUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+class PaymentDestroyAPIView(generics.DestroyAPIView):
+    queryset = Payment.objects.all()
 
